3ff860713da5e20c100b18e4daa4fd0e5484717f
amith-exe <amith1biju@gmail.com>
2026-03-16 02:15:42 +0530

    added main function to enter user input and error validaion of input

--- level-1_rover_distance/include/Rover.hpp
+++ level-1_rover_distance/include/Rover.hpp
@@ -1,11 +1,15 @@
 #pragma once
 #include<tuple>
+/*
+ Decalaration of the function needed for the calculation
+             for better structure and reusability of code
+*/
 
 class Rover{
 public:
-    static double calculatingDistance(
-     std::tuple<double,double>& origin,
-     std::tuple<double,double>& destination
+    static double calculateDistance(
+      std::tuple<double,double>& origin,
+      std::tuple<double,double>& destination
     );
      static double calculateTime(
         double distance,
